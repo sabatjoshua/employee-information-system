dee32b14bb1051985778bfa4b20b0b3c811eca07
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 11:46:11 +0800

    Add user creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/Users/CreateUserValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Users
+{
+    public sealed class CreateUserValidator
+        : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreateUserValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeInformationSystem.Application.Features.Users;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreateUserValidatorTests
+    {
+        private readonly CreateUserValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new CreateUserCommand(
+                Guid.Empty,
+                string.Empty,
+                "123");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.UserName);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Password);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new CreateUserCommand(
+                Guid.NewGuid(),
+                "john.doe",
+                "Password123");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
