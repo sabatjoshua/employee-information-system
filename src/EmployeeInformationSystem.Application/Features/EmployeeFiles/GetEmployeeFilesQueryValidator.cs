9e9a066017ce2ebb3d235dfe68df6a1224cbc5f6
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 16:25:57 +0800

    Add employee file query validation

---
+++ src/EmployeeInformationSystem.Application/Features/EmployeeFiles/GetEmployeeFilesQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.EmployeeFiles
+{
+    public sealed class GetEmployeeFilesQueryValidator
+        : AbstractValidator<GetEmployeeFilesQuery>
+    {
+        public GetEmployeeFilesQueryValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/GetEmployeeFilesQueryValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeInformationSystem.Application.Features.EmployeeFiles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class GetEmployeeFilesQueryValidatorTests
+    {
+        private readonly GetEmployeeFilesQueryValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenEmployeeIdIsInvalid()
+        {
+            var query = new GetEmployeeFilesQuery(
+                Guid.Empty);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenEmployeeIdIsValid()
+        {
+            var query = new GetEmployeeFilesQuery(
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
