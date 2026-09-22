d67897636ad970c3dcdadfa064bda5e59bc01df5
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 16:18:07 +0800

    Add employee role validation

---
+++ src/EmployeeInformationSystem.Application/Features/EmployeeRoles/UpdateEmployeeRoleValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.EmployeeRoles
+{
+    public sealed class UpdateEmployeeRoleValidator
+        : AbstractValidator<UpdateEmployeeRoleCommand>
+    {
+        public UpdateEmployeeRoleValidator()
+        {
+            RuleFor(x => x.EmployeeRoleId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.RoleId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdateEmployeeRoleValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeInformationSystem.Application.Features.EmployeeRoles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdateEmployeeRoleValidatorTests
+    {
+        private readonly UpdateEmployeeRoleValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdateEmployeeRoleCommand(
+                Guid.Empty,
+                Guid.Empty,
+                Guid.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeRoleId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.RoleId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new UpdateEmployeeRoleCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
