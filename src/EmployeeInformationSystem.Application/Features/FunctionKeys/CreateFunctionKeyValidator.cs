b219ed98689cc5d345ab17a6b946d6e19c7656f6
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:45:37 +0800

    Add function key creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/FunctionKeys/CreateFunctionKeyValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.FunctionKeys
+{
+    public sealed class CreateFunctionKeyValidator
+        : AbstractValidator<CreateFunctionKeyCommand>
+    {
+        public CreateFunctionKeyValidator()
+        {
+            RuleFor(x => x.FunctionCode)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.DisplayName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Remarks)
+                .MaximumLength(500)
+                .When(x => !string.IsNullOrWhiteSpace(x.Remarks));
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreateFunctionKeyValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeInformationSystem.Application.Features.FunctionKeys;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreateFunctionKeyValidatorTests
+    {
+        private readonly CreateFunctionKeyValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new CreateFunctionKeyCommand(
+                string.Empty,
+                string.Empty,
+                new string('A', 501));
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FunctionCode);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.DisplayName);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Remarks);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new CreateFunctionKeyCommand(
+                "EMPLOYEE_VIEW",
+                "View Employees",
+                "View employee information");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
