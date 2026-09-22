b29a115de0b70782c871f42a26b64a13ea3bf381
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:29:06 +0800

    Add position validation

---
+++ src/EmployeeInformationSystem.Application/Features/Positions/UpdatePositionValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Positions
+{
+    public sealed class UpdatePositionValidator
+        : AbstractValidator<UpdatePositionCommand>
+    {
+        public UpdatePositionValidator()
+        {
+            RuleFor(x => x.PositionId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.DepartmentId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdatePositionValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeInformationSystem.Application.Features.Positions;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdatePositionValidatorTests
+    {
+        private readonly UpdatePositionValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdatePositionCommand(
+                Guid.Empty,
+                string.Empty,
+                Guid.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.PositionId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.DepartmentId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new UpdatePositionCommand(
+                Guid.NewGuid(),
+                "Senior Software Engineer",
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
