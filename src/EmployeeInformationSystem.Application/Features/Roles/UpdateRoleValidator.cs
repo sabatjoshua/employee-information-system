c51bf4be361bbd19798cf6ba8959ce5311e343ca
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:42:36 +0800

    Add role validation

---
+++ src/EmployeeInformationSystem.Application/Features/Roles/UpdateRoleValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Roles
+{
+    public sealed class UpdateRoleValidator
+        : AbstractValidator<UpdateRoleCommand>
+    {
+        public UpdateRoleValidator()
+        {
+            RuleFor(x => x.RoleId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500)
+                .When(x => !string.IsNullOrWhiteSpace(x.Description));
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdateRoleValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeInformationSystem.Application.Features.Roles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdateRoleValidatorTests
+    {
+        private readonly UpdateRoleValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdateRoleCommand(
+                Guid.Empty,
+                string.Empty,
+                new string('A', 501));
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.RoleId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Description);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new UpdateRoleCommand(
+                Guid.NewGuid(),
+                "HR Administrator",
+                null);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
