e6054df036e48ab6a3cf849bc91261f0fe785a34
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:53:30 +0800

    Add function key update validation

---
+++ src/EmployeeInformationSystem.Application/Features/FunctionKeys/UpdateFunctionKeyValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.FunctionKeys
+{
+    public sealed class UpdateFunctionKeyValidator
+        : AbstractValidator<UpdateFunctionKeyCommand>
+    {
+        public UpdateFunctionKeyValidator()
+        {
+            RuleFor(x => x.FunctionKeyId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.FunctionCode)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.DisplayName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Remarks)
+                .MaximumLength(500)
+                .When(x => !string.IsNullOrWhiteSpace(x.Remarks));
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdateFunctionKeyValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeInformationSystem.Application.Features.FunctionKeys;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdateFunctionKeyValidatorTests
+    {
+        private readonly UpdateFunctionKeyValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdateFunctionKeyCommand(
+                Guid.Empty,
+                string.Empty,
+                string.Empty,
+                new string('A', 501));
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FunctionKeyId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FunctionCode);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.DisplayName);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Remarks);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new UpdateFunctionKeyCommand(
+                Guid.NewGuid(),
+                "EMPLOYEE_VIEW",
+                "View Employees",
+                "View employee information");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
