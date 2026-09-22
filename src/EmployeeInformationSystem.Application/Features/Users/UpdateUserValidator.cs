30f243b699c36b65e91a46804d1bf2c2fd10074f
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 12:08:03 +0800

    Add user update validation

---
+++ src/EmployeeInformationSystem.Application/Features/Users/UpdateUserValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Users
+{
+    public sealed class UpdateUserValidator
+        : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Password)
+                .MinimumLength(8)
+                .When(x => !string.IsNullOrWhiteSpace(x.Password));
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdateUserValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeInformationSystem.Application.Features.Users;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdateUserValidatorTests
+    {
+        private readonly UpdateUserValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdateUserCommand(
+                Guid.Empty,
+                Guid.Empty,
+                string.Empty,
+                "123",
+                false,
+                false);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.UserId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.UserName);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Password);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenPasswordIsNotProvided()
+        {
+            var command = new UpdateUserCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "john.doe",
+                null,
+                false,
+                false);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
