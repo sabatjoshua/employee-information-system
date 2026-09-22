0933a25c2c45a64edfa92fdba8adffc2afd6bcd8
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 16:05:06 +0800

    Add employee role creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/EmployeeRoles/CreateEmployeeRoleValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.EmployeeRoles
+{
+    public sealed class CreateEmployeeRoleValidator
+        : AbstractValidator<CreateEmployeeRoleCommand>
+    {
+        public CreateEmployeeRoleValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.RoleId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreateEmployeeRoleValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeInformationSystem.Application.Features.EmployeeRoles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreateEmployeeRoleValidatorTests
+    {
+        private readonly CreateEmployeeRoleValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new CreateEmployeeRoleCommand(
+                Guid.Empty,
+                Guid.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.RoleId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new CreateEmployeeRoleCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
