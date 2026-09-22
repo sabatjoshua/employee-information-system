1739ae5e0743b3a79c83dbf200f0fda05288ed05
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 16:34:42 +0800

    Add employee file download validation

---
+++ src/EmployeeInformationSystem.Application/Features/EmployeeFiles/DownloadEmployeeFileQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.EmployeeFiles
+{
+    public sealed class DownloadEmployeeFileQueryValidator
+        : AbstractValidator<DownloadEmployeeFileQuery>
+    {
+        public DownloadEmployeeFileQueryValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.FileId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/DownloadEmployeeFileQueryValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeInformationSystem.Application.Features.EmployeeFiles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class DownloadEmployeeFileQueryValidatorTests
+    {
+        private readonly DownloadEmployeeFileQueryValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var query = new DownloadEmployeeFileQuery(
+                Guid.Empty,
+                Guid.Empty);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FileId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenQueryIsValid()
+        {
+            var query = new DownloadEmployeeFileQuery(
+                Guid.NewGuid(),
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
