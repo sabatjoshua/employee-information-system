16bf5ebe24441e71d01f8afd4bc4eb73c1548ab9
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 16:21:11 +0800

    Add employee file upload validation

---
+++ src/EmployeeInformationSystem.Application/Features/EmployeeFiles/UploadEmployeeFileValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.EmployeeFiles
+{
+    public sealed class UploadEmployeeFileValidator
+        : AbstractValidator<UploadEmployeeFileCommand>
+    {
+        public UploadEmployeeFileValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.FileStream)
+                .NotNull();
+
+            RuleFor(x => x.OriginalFileName)
+                .NotEmpty();
+
+            RuleFor(x => x.ContentType)
+                .NotEmpty();
+
+            RuleFor(x => x.FileSize)
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UploadEmployeeFileValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeInformationSystem.Application.Features.EmployeeFiles;
+using FluentValidation.TestHelper;
+using System.Text;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UploadEmployeeFileValidatorTests
+    {
+        private readonly UploadEmployeeFileValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UploadEmployeeFileCommand(
+                Guid.Empty,
+                null!,
+                string.Empty,
+                string.Empty,
+                0);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.EmployeeId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FileStream);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.OriginalFileName);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.ContentType);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.FileSize);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            using var stream = new MemoryStream(
+                Encoding.UTF8.GetBytes("test file"));
+
+            var command = new UploadEmployeeFileCommand(
+                Guid.NewGuid(),
+                stream,
+                "document.pdf",
+                "application/pdf",
+                stream.Length);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
