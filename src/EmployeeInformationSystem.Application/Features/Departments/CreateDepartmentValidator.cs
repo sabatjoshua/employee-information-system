894c552153d9cf6f46a36ecf69a80bde11208f87
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:13:11 +0800

    Add department creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/Departments/CreateDepartmentValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Departments
+{
+    public sealed class CreateDepartmentValidator
+        : AbstractValidator<CreateDepartmentCommand>
+    {
+        public CreateDepartmentValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreateDepartmentValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeInformationSystem.Application.Features.Departments;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreateDepartmentValidatorTests
+    {
+        private readonly CreateDepartmentValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenNameIsInvalid()
+        {
+            var command = new CreateDepartmentCommand(
+                string.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenNameIsValid()
+        {
+            var command = new CreateDepartmentCommand(
+                "Human Resources");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
