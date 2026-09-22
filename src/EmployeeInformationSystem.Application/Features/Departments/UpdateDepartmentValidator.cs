40239be4f647631879bf4a19db20b58f34af2b99
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:18:20 +0800

    Add department validation

---
+++ src/EmployeeInformationSystem.Application/Features/Departments/UpdateDepartmentValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Departments
+{
+    public sealed class UpdateDepartmentValidator
+        : AbstractValidator<UpdateDepartmentCommand>
+    {
+        public UpdateDepartmentValidator()
+        {
+            RuleFor(x => x.DepartmentId)
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/UpdateDepartmentValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeInformationSystem.Application.Features.Departments;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class UpdateDepartmentValidatorTests
+    {
+        private readonly UpdateDepartmentValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new UpdateDepartmentCommand(
+                Guid.Empty,
+                string.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.DepartmentId);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new UpdateDepartmentCommand(
+                Guid.NewGuid(),
+                "Human Resources");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
