0c13d28d4900910ad26982e3542b56a131153c35
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:33:45 +0800

    Add role creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/Roles/CreateRoleValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Roles
+{
+    public sealed class CreateRoleValidator
+        : AbstractValidator<CreateRoleCommand>
+    {
+        public CreateRoleValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500)
+                .When(x => !string.IsNullOrWhiteSpace(x.Description));
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreateRoleValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeInformationSystem.Application.Features.Roles;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreateRoleValidatorTests
+    {
+        private readonly CreateRoleValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new CreateRoleCommand(
+                string.Empty,
+                new string('A', 501));
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Description);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new CreateRoleCommand(
+                "HR Administrator",
+                "Manages employees and user accounts.");
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
