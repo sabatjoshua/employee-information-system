517e7b03eacfdcc12c065b9c74d18cb651c3ef6e
Joshua Sabat <sabat_joshua@yahoo.com>
2026-09-22 15:24:57 +0800

    Add position creation validation

---
+++ src/EmployeeInformationSystem.Application/Features/Positions/CreatePositionValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace EmployeeInformationSystem.Application.Features.Positions
+{
+    public sealed class CreatePositionValidator
+        : AbstractValidator<CreatePositionCommand>
+    {
+        public CreatePositionValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.DepartmentId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ tests/EmployeeInformationSystem.Tests/CreatePositionValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeInformationSystem.Application.Features.Positions;
+using FluentValidation.TestHelper;
+
+namespace EmployeeInformationSystem.Tests
+{
+    public class CreatePositionValidatorTests
+    {
+        private readonly CreatePositionValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldFail_WhenRequiredFieldsAreInvalid()
+        {
+            var command = new CreatePositionCommand(
+                string.Empty,
+                Guid.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.Name);
+
+            result.ShouldHaveValidationErrorFor(
+                x => x.DepartmentId);
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenCommandIsValid()
+        {
+            var command = new CreatePositionCommand(
+                "Software Engineer",
+                Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
